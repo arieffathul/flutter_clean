6b86b48e59106130a7b944834149daaa8d18837a
arieffathul <ariefulum2005@gmail.com>
2025-04-29 01:55:47 +0700

    fix(auth): Auto-Redirect-Verification

--- lib/core/routes/routes.dart
+++ lib/core/routes/routes.dart
@@ -7,6 +7,8 @@ import 'package:clean_flutter/features/product/presentation/pages/product_pages%
 import 'package:clean_flutter/features/product/presentation/pages/product_pages.dart';
 import 'package:clean_flutter/features/suplier/presentation/pages/suplier_pages.dart';
 import 'package:clean_flutter/features/type_product/presentation/pages/type_pages.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 // import 'package:firebase_auth/firebase_auth.dart';
 // import 'package:flutter/foundation.dart';
 import 'package:go_router/go_router.dart';
@@ -14,11 +16,50 @@ import 'package:go_router/go_router.dart';
 // class AuthNotifier extends ChangeNotifier {
 //   final FirebaseAuth auth = FirebaseAuth.instance;
 // }
+class AuthNotifier extends ChangeNotifier {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  AuthNotifier() {
+    _auth.authStateChanges().listen((User? user) {
+      notifyListeners();
+    });
+  }
+  bool get isLoggedIn => _auth.currentUser != null;
+}
+
+final authNotifier = AuthNotifier();
 
 class MyRouter {
   get router => GoRouter(
         initialLocation: '/',
-        // redirect: (context, state) {},
+
+        redirect: (context, state) {
+          final isLoggedIn = authNotifier.isLoggedIn;
+          final isLoggingIn = state.matchedLocation == '/' ||
+              state.matchedLocation == '/register';
+
+          if (!isLoggedIn && !isLoggingIn) {
+            return '/';
+          }
+          if (isLoggedIn && isLoggingIn) {
+            return '/produk';
+          }
+          return null;
+        },
+
+        //   final routesBeforeLogin = ['/', '/register'];
+        //   final routesAfterLogin = ['/produk'];
+
+        //   if (isLoggedIn && routesBeforeLogin.contains(state.matchedLocation)) {
+        //     return '/produk';
+        //   }
+
+        //   if (isLoggedIn && routesAfterLogin.contains(state.matchedLocation)) {
+        //     return '/';
+        //   }
+        //   return null;
+        // },
+        // refreshListenable: authNotifier,
         routes: [
           GoRoute(
             path: '/',
